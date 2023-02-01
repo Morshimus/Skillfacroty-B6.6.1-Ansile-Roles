3851d48bcd9437ad8f28db160b2347431a74156a
Daniel Dalavurak <morsh110792@yahoo.com>
2023-02-01 18:11:45 +0300

    ok

---
+++ actions.ps1
@@ -0,0 +1,68 @@
+function ansible {
+    param (
+        [Parameter(Mandatory=$False)]
+        [string]$distr = "Ubuntu-20.04",
+        [Parameter(Mandatory=$False)]
+        [String]$user = "morsh92",
+        [Parameter(Mandatory=$False)]
+        [String]$server = "lemp",
+        [Parameter(Mandatory=$False)]
+        [String]$invFile = "./yandex_cloud.ini",
+        [Parameter(Mandatory=$False)]
+        [String]$privateKey = "~/.ssh/morsh_bastion_SSH",
+        [Parameter(Mandatory=$False,Position=0)]
+        [String]$args
+    )
+    wsl -d $distr -u $user -e ansible $server --inventory-file "$invFile" --private-key $privateKey $args
+} 
+
+Set-Alias ansible-playbook ansiblePlaybook
+function ansiblePlaybook {
+    param (
+        [Parameter(Mandatory=$False)]
+        [string]$distr = "Ubuntu-20.04",
+        [Parameter(Mandatory=$False)]
+        [String]$user = "morsh92",        
+        [Parameter(Mandatory=$False)]
+        [String]$server = "lemp",
+        [Parameter(Mandatory=$False)]
+        [String]$invFile = "./yandex_cloud.ini",
+        [Parameter(Mandatory=$False)]
+        [String]$privateKey = "~/.ssh/morsh_bastion_SSH",
+        [Parameter(Mandatory=$False)]
+        [String]$Playbook = "./provisioning_web_admin.yaml",
+        [Parameter(Mandatory=$False,Position=0)]
+        [string]$fileSecrets = '~/.vault_pass',
+        [Parameter(Mandatory=$False,Position=0)]
+        [switch]$tagInit,
+        [Parameter(Mandatory=$False,Position=0)]
+        [switch]$tagDrop
+    )
+
+   if($tagInit){$param='--tags';$tag = "init postfix"}elseif($tagDrop){$param='--tags';$tag = "drop postfix"}
+
+    wsl -d $distr -u $user -e ansible-playbook  -i "$invFile" --private-key $privateKey -e '@secrets' --vault-password-file=$fileSecrets  $Playbook  $param $tag
+} 
+
+Set-Alias ansible-vault ansibleVault
+function ansibleVault {
+    param (
+        [Parameter(Mandatory=$False)]
+        [string]$distr = "Ubuntu-20.04",
+        [Parameter(Mandatory=$False)]
+        [String]$user = "morsh92",
+        [Parameter(Mandatory=$False,Position=0)]
+        [String]$action = 'encrypt',
+        [Parameter(Mandatory=$False,Position=0)]
+        [String]$file = 'provisioning_web_admin.yaml',
+        [Parameter(Mandatory=$False,Position=0)]
+        [switch]$ask,
+        [Parameter(Mandatory=$False,Position=0)]
+        [string]$fileSecrets = '~/.vault_pass'
+
+    )
+    
+    if($ask){$passwd = "--ask-vault-pass"}
+
+    wsl -d $distr -u $user -e ansible-vault $action --vault-password-file=$fileSecrets $passwd $file
+} 
